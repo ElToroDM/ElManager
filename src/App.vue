110ee9d0ae15a1e1114d64e77b8ce91fc8674479
toro <dmfraga@gmail.com>
2023-01-09 13:46:01 -0300

    Branch highlighting. Drag & drop changes/fixes

--- src/App.vue
+++ src/App.vue
@@ -2,74 +2,44 @@
 import { ref } from 'vue'
 import itemsList from './itemsList.vue'
 
-
 // const items = ref([
-//   { name: 'AP01', id: 1 },
-//   { name: 'Armar', id: 2 },
-//   { name: 'Movimiento', id: 3 },
-//   { name: 'Zócalo GU10', id: 4 },
-//   { name: 'Base', id: 5 },
-//   { name: 'Terminación', id: 6 },
-//   { name: 'Mecanizado', id: 7 },
-//   { name: 'Chapa nº20 160x121', id: 8 },
-//   { name: 'Cilindro', id: 9 },
-//   { name: 'Terminación', id: 10 },
-//   { name: 'Mecanizado', id: 11 },
-//   { name: 'Cilindro repujado', id: 12 }
-// ])
-
-// const items = ref([
-//   { "name": "AP01", "id": 1, "level": 0, "successors": [2] },
-//   { "name": "Armar", "id": 2, "level": 1, "predecessors": [1], "successors": [3, 4, 5, 9] },
-//   { "name": "Movimiento", "id": 3, "level": 2, "predecessors": [2] },
-//   { "name": "Zócalo GU10", "id": 4, "level": 2, "predecessors": [2] },
-//   { "name": "Base", "id": 5, "level": 2, "successors": [6], "predecessors": [2] },
-//   { "name": "Terminación", "id": 6, "level": 3, "successors": [7], "predecessors": [5] },
-//   { "name": "Mecanizado", "id": 7, "level": 4, "successors": [8], "predecessors": [6] },
-//   { "name": "Chapa nº20 160x121", "id": 8, "level": 5, "predecessors": [7] },
-//   { "name": "Cilindro", "id": 9, "level": 2, "successors": [10], "predecessors": [2] },
-//   { "name": "Terminación", "id": 10, "level": 3, "successors": [11], "predecessors": [9] },
-//   { "name": "Mecanizado", "id": 11, "level": 4, "successors": [12], "predecessors": [10] },
-//   { "name": "Cilindro repujado", "id": 12, "level": 5, "predecessors": [11] }
-// ])
-
-// const items = ref([
-//   { "name": "AP01", "id": 1, "level": 0, "successors": [2], "cost": 1 },
-//   { "name": "Armar", "id": 2, "level": 1, "successors": [3, 4, 5, 9], "cost": 1 },
+//   { "name": "AP01", "id": 1, "level": 0, "cost": 1 },
+//   { "name": "Armar", "id": 2, "level": 1, "cost": 1 },
 //   { "name": "Movimiento", "id": 3, "level": 2, "cost": 1 },
 //   { "name": "Zócalo GU10", "id": 4, "level": 2, "cost": 1 },
-//   { "name": "Base", "id": 5, "level": 2, "successors": [6], "cost": 1 },
-//   { "name": "Terminación", "id": 6, "level": 3, "successors": [7], "cost": 1 },
-//   { "name": "Mecanizado", "id": 7, "level": 4, "successors": [8], "cost": 1 },
+//   { "name": "Base", "id": 5, "level": 2, "cost": 1 },
+//   { "name": "Terminación", "id": 6, "level": 3, "cost": 1 },
+//   { "name": "Mecanizado", "id": 7, "level": 4, "cost": 1 },
 //   { "name": "Chapa nº20 160x121", "id": 8, "level": 5, "cost": 3 },
-//   { "name": "Cilindro", "id": 9, "level": 2, "successors": [10], "cost": 1 },
-//   { "name": "Terminación", "id": 10, "level": 3, "successors": [11], "cost": 1 },
-//   { "name": "Mecanizado", "id": 11, "level": 4, "successors": [12], "cost": 2 },
+//   { "name": "Cilindro", "id": 9, "level": 2, "cost": 1 },
+//   { "name": "Terminación", "id": 10, "level": 3, "cost": 1 },
+//   { "name": "Mecanizado", "id": 11, "level": 4, "cost": 2 },
 //   { "name": "Cilindro repujado", "id": 12, "level": 5, "cost": 3 }
 // ])
 
 const items = ref([
-  { "name": "AP01", "id": 1, "level": 0, "cost": 1 },
-  { "name": "Armar", "id": 2, "level": 1, "cost": 1 },
-  { "name": "Movimiento", "id": 3, "level": 2, "cost": 1 },
-  { "name": "Zócalo GU10", "id": 4, "level": 2, "cost": 1 },
-  { "name": "Base", "id": 5, "level": 2, "cost": 1 },
-  { "name": "Terminación", "id": 6, "level": 3, "cost": 1 },
-  { "name": "Mecanizado", "id": 7, "level": 4, "cost": 1 },
-  { "name": "Chapa nº20 160x121", "id": 8, "level": 5, "cost": 3 },
-  { "name": "Cilindro", "id": 9, "level": 2, "cost": 1 },
-  { "name": "Terminación", "id": 10, "level": 3, "cost": 1 },
-  { "name": "Mecanizado", "id": 11, "level": 4, "cost": 2 },
-  { "name": "Cilindro repujado", "id": 12, "level": 5, "cost": 3 }
-  //,  { "name": "*", "id": -1, "level": -1}
+  { "name": "Panqueque de dulce de leche", "id": 1, "level": 0, "cost": 1, "open": true },
+  { "id": 732785649504646, "name": "Enrollar", "level": 1, "cost": 2, "open": true },
+  { "id": 732787039516608, "name": "Panqueque", "level": 2, "cost": 2, "open": true },
+  { "id": 732787228074946, "name": "Cocinar vuelta y vuelta", "level": 3, "cost": 2, "open": true },
+  { "id": 732787388636990, "name": "Pasta", "level": 4, "cost": 2, "open": true },
+  { "id": 732787473433670, "name": "Batir", "level": 5, "cost": 2, "open": true },
+  { "id": 732787615515042, "name": "Huevos", "level": 6, "cost": 2 },
+  { "id": 732787613195910, "name": "Manteca derretida", "level": 6, "cost": 2 },
+  { "id": 732787611591662, "name": "Leche", "level": 6, "cost": 2 },
+  { "id": 732787609910024, "name": "Harina", "level": 6, "cost": 2 },
+  { "id": 732787604156978, "name": "Esencia de vainilla", "level": 6, "cost": 2 },
+  { "id": 732787571510938, "name": "Sal", "level": 6, "cost": 2 },
+  { "id": 732787108951490, "name": "Dulce de leche", "level": 2, "cost": 2 }
 ])
 </script>
 
 <template>
-  <itemsList :items="items"></itemsList>
-
-  <div style="position:absolute; bottom:0;height: 49vh; overflow-y: scroll;width:100%;">
-    <div v-for="item in items" style="overflow:hidden; white-space: nowrap;"> {{ item }}</div>
+  <div style="height: 70vh; overflow-y: scroll; overflow-x:hidden;" onselectstart='return false;'>
+    <itemsList :items="items"></itemsList>
+  </div>
+  <div style="position:absolute; bottom:0;height: 30vh; overflow-y: scroll;width:100%;">
+    <div v-for="item in items" style="overflow:hidden; white-space: nowrap;"> {{ item }},</div>
   </div>
 </template>
 
@@ -81,7 +51,9 @@ body {
   background-color: rgb(255, 253, 228);
   color: rgb(12, 12, 12);
 }
-body, input{
+
+body,
+input {
   font-family: "Clear Sans", "Helvetica Neue", Arial, sans-serif;
   font-size: 2vh;
 }
--- src/itemsList.vue
+++ src/itemsList.vue
@@ -1,86 +1,66 @@
 <script setup>
 /* TODO:
+scroll when dragging upper or lower
 edit props structure
 edit props
 undo actions: useManualRefHistory? or Pinia? ...or withouts libs
 save data
-alternative to :hover buttons because of touch screens
-move item buttons for touch because of touch screens functions like scroll, etc
+(touch) fix double clic or make button for rename
+(touch) move item buttons to avoid conflicts with touch screens functions like scroll, gestures, etc
 */
 import { ref, reactive, computed, onMounted, nextTick } from 'vue'
-const props = defineProps({items: Object})
-const itemrefs = ref([])
-const states = reactive({})
+const props = defineProps({ items: Object })
+const itemref = ref([])
+const state = reactive({})
 
 //______________________________________________________________________________
 // DRAG & DROP
 //______________________________________________________________________________
 const drag = reactive({})
-
 //______________________________________________________________________________
 function onDragStart(event, item) {
   drag.sourceItemId = item.id
-
+  // Reset horizontal drag threshold
+  drag.x = event.pageX || event.touches[0].pageX
+  // Create a copy of the items to move
   const sourceItemIndex = props.items.indexOf(item)
   const lastSuccessorIndex = getLastSuccessorIndex(item)
-  // Create a copy of the items to move
   drag.itemsCopy = props.items.slice(sourceItemIndex, lastSuccessorIndex + 1)
   // Mark original items as "dragging"
   for (let i = sourceItemIndex; i <= lastSuccessorIndex; i++) props.items[i].dragging = true
-
-  drag.x = event.pageX || event.touches[0].pageX
 }
-
 //______________________________________________________________________________
 function onDragEnd() {
-  // drag.info = ''
-  // delete dragging props from items
+  // Delete dragging props from items
   props.items.forEach(item => delete item.dragging)
   // Delete drag props
   for (const prop in drag) delete drag[prop]
-
 }
 //______________________________________________________________________________
 function onDragOver(event, item) {
-  if (!props.mouseDown) return
-
-  // Adjust item whith touch events (touchMove targets original item, even when the touch is over another item)
-  if (event.touches) {
-    // Get the element at the touch point
-    let element = document.elementFromPoint(event.touches[0].clientX, event.touches[0].clientY)//(posX, posY)
-    let index = itemrefs.value.indexOf(element)
-    // If element is not a item, check if it is child of item
-    if (index === -1) {
-      if (!element) return
-      index = itemrefs.value.indexOf(element.parentNode)
-      if (index === -1) return
-    }
-    item = props.items[index]
-    // drag.info = index + ' item: ' + item.id + ' x:' + posX + ' y:' + posY
-  }
-
-  // posX (for horizontal dragging)
-  const posX = event.pageX || event.touches[0].pageX
-  // drag.info = ' item: ' + item.id + ' x:' + posX //+ ' y:' + posY
   const sourceItemId = drag.sourceItemId
   const sourceItem = props.items.find(x => x.id == sourceItemId)
   const sourceItemIndex = props.items.indexOf(sourceItem)
   const lastSuccessorIndex = getLastSuccessorIndex(sourceItem)
   let targetItemIndex = props.items.indexOf(item)
   let levelDifference = item.level - sourceItem.level
-
+  let posX; if (event.touches) { posX = event.touches[0].pageX } else { posX = event.pageX }
+  let posY; if (event.touches) { posY = event.touches[0].pageY } else { posY = event.pageY }
   //______________________________________
   // Handle horizontal dragging, if any, for level adjustment and return:
   //______________________________________
+  const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0) / 100
+  const dragThresholdX = 1 * vh
   if (targetItemIndex === sourceItemIndex) {
     if (targetItemIndex === 0) return
-    if (posX > drag.x + 20) {
-      //drag.info = ' item: ' + item.id + ' x:' + posX
+    if (posX > drag.x + dragThresholdX) {
       // Move to the right
       if (props.items[targetItemIndex].level === props.items[targetItemIndex - 1].level) levelDifference++
       for (let i = targetItemIndex; i <= lastSuccessorIndex; i++) props.items[i].level += levelDifference
       drag.x = posX
-    } else if (posX < drag.x - 20) {
+      return
+    } else if (posX < drag.x - dragThresholdX) {
+      drag.x = posX
       // Move to the left
       if (props.items[targetItemIndex].level - props.items[targetItemIndex - 1].level === 1) {
         if (lastSuccessorIndex + 1 < props.items.length) {
@@ -88,15 +68,30 @@ function onDragOver(event, item) {
         }
         levelDifference--
         for (let i = targetItemIndex; i <= lastSuccessorIndex; i++) props.items[i].level += levelDifference
-        drag.x = posX
       }
+      return
     }
-    return
-    // End of horizontal handling
   }
+  
+  // drag.info = ' item: ' + item.id + ' drag.x:' + drag.x + ' posX:' + posX
+
   //______________________________________
   // Vertical dragging for moving items
   //______________________________________
+    // Adjust item whith touch events (touchMove targets original item, even when the touch is over another item)
+  // if (event.touches) {
+  // Get the element at the touch point
+  let element = document.elementFromPoint(posX, posY)
+  let index = itemref.value.indexOf(element)
+  // If element is not a item, check if it is child of item
+  if (index === -1) {
+    if (!element) return
+    index = itemref.value.indexOf(element.parentNode)
+    if (index === -1) return
+  }
+  item = props.items[index]
+  targetItemIndex = props.items.indexOf(item)
+  levelDifference = item.level - sourceItem.level
   // If item target is on the same branch, change it for the item that immediately follows the branch
   if (targetItemIndex > sourceItemIndex && targetItemIndex <= lastSuccessorIndex) {
     targetItemIndex += lastSuccessorIndex - sourceItemIndex
@@ -104,7 +99,6 @@ function onDragOver(event, item) {
     item = props.items[targetItemIndex]
     levelDifference = item.level - sourceItem.level
   }
-
   // Adjust target position if below than source in the tree (indexes are higher when position is below)
   if (targetItemIndex > sourceItemIndex) {
     targetItemIndex++
@@ -114,34 +108,35 @@ function onDragOver(event, item) {
       levelDifference++
     }
   }
-
+  // Don't vertical drag to the same item
+  if (targetItemIndex === sourceItemIndex) return
   // Adjust levels of items to move
   drag.itemsCopy = drag.itemsCopy.map(e => ({ ...e, level: e.level + levelDifference }))
   // Insert items in new position
   props.items.splice(targetItemIndex, 0, ...drag.itemsCopy);
   //Remove sourceItem (and successors)
   removeItem(sourceItem)
+  // Vertical dragging made, reset horizontal dragging
+  drag.x = posX
 }
 
 //______________________________________________________________________________
 // ITEMS MANIPULATION
 //______________________________________________________________________________
 function newItemId() {
-  return (Date.now() + Math.random()) * 10000
+  // return (Date.now() + Math.random()) * 10000
+  return (Date.now()+ Math.random()) *10000 % 1000000000000000
 }
-
 //______________________________________________________________________________
 function insertItem(item) {
   const targetItemIndex = props.items.indexOf(item)
   props.items.splice(targetItemIndex + 1, 0, { id: newItemId(), name: 'New item', level: item.level + 1, cost: 2 })
 }
-
 //______________________________________________________________________________
 function removeItem(item) {
   const targetItemIndex = props.items.indexOf(item)
   props.items.splice(targetItemIndex, getLastSuccessorIndex(item) - targetItemIndex + 1)
 }
-
 //______________________________________________________________________________
 function getLastSuccessorIndex(item) {
   const targetItemIndex = props.items.indexOf(item)
@@ -152,12 +147,10 @@ function getLastSuccessorIndex(item) {
   }
   return lastSuccessorIndex
 }
-
 //______________________________________________________________________________
 function itemTotalCost(item) {
   return props.items.slice(props.items.indexOf(item), getLastSuccessorIndex(item) + 1).reduce((a, b) => a + b.cost, 0)
 }
-
 //______________________________________________________________________________
 function isFolder(item) {
   try {
@@ -166,7 +159,6 @@ function isFolder(item) {
     return false
   }
 }
-
 //______________________________________________________________________________
 function toggleOpenFolder(item) {
   if (item.open === false) {
@@ -175,7 +167,6 @@ function toggleOpenFolder(item) {
     item.open = false
   }
 }
-
 //______________________________________________________________________________
 let previousOpenFolderLevel = 0
 function isInOpenFolder(item) {
@@ -198,101 +189,104 @@ function isInOpenFolder(item) {
 }
 
 //______________________________________________________________________________
-/*function unregisterFromPredeccessor(itemId) {
-  // Search predeccessor item and unregister from it
-  for (let i = 0; i < props.items.length; i++) {
-    if (props.items[i].successors) {
-      for (let s = 0; s < props.items[i].successors.length; s++) {
-        if (props.items[i].successors[s] === itemId) {
-          // Predeccessor found, now unregister and return
-          if (props.items[i].successors.length <= 1) {
-            delete props.items[i].successors
-          } else {
-            props.items[i].successors.splice(s, 1)
-          }
-          return
-        }
-      }
-    }
-  }
-}*/
-
-//______________________________________________________________________________
 // ITEMS EVENTS
 //______________________________________________________________________________
 document.addEventListener('mouseup', onMouseUp)
 document.addEventListener('touchend', onMouseUp)
 document.addEventListener('mousedown', onMouseDown)
+document.addEventListener('mousemove', onMouseMove)
 //______________________________________________________________________________
 function onMouseDown(event, item) {
-  props.mouseDown = true
+  selectedItemStyleUpdate(item)
+  event.stopPropagation()
+  state.mouseDown = true
   if (item) onDragStart(event, item)
 }
 //______________________________________________________________________________
 function onMouseUp() {
-  props.mouseDown = false
+  state.mouseDown = false
   onDragEnd()
 }
 //______________________________________________________________________________
 function onDblClicK(item) {
-  props.itemEditing = item.id
+  state.itemEditing = item.id
   nextTick(() => {
     const itemInput = document.getElementById('itemEditInput')
     itemInput.focus()
+    itemInput.select()
   })
 }
 //______________________________________________________________________________
-function onMouseMove(event) {
+function onMouseMove(event, item) {
   // drag.info = event.clientY + ' ' + event.pageY + ' ' + event.screenY
+  event.stopPropagation()
+  // Items highlights on hover
+  selectedItemStyleUpdate(item)
+  // Handle drags
+  if (item && state.mouseDown) onDragOver(event, item)
+}
+//______________________________________________________________________________
+function itemHighlight(item) {
+  if (item.highlight) return 'itemHighlight' + item.highlight
+}
+//______________________________________________________________________________
+function selectedItemStyleUpdate(item) {
+  if (state.mouseDown) return
+  // Delete old highlights
+  props.items.forEach(item => delete item.highlight)
+  if (!item) return
+  // Add new highlights props
+  const sourceItemIndex = props.items.indexOf(item)
+  const lastSuccessorIndex = getLastSuccessorIndex(item)
+  item.highlight = 1
+  for (let i = sourceItemIndex + 1; i <= lastSuccessorIndex; i++) props.items[i].highlight = 2
 }
 //______________________________________________________________________________
 </script>
 
 <template>
-  <div style="height: 49vh; overflow-y: scroll;">
-    <template v-for="item in items">
-      <div v-if="isInOpenFolder(item)" :style="{ 'padding-left': item.level * 2 + 'vh' }" class="itemLine"
-        ref="itemrefs" :class="{ dragging: item.dragging }" @mousedown="onMouseDown($event, item)"
-        @mousemove="onDragOver($event, item)" @touchstart="onMouseDown($event, item)"
-        @touchmove="onDragOver($event, item)">
-        <span :class="{ invisible: !isFolder(item) }" @click="toggleOpenFolder(item)" class="isFolder">
-          {{ item.open === false ? '&#62;' : '&#709;' }}</span>
-        <input v-if="props.itemEditing == item.id" v-model="item.name" @blur="props.itemEditing = false"
-          @keyup.enter="props.itemEditing = false" id="itemEditInput" />
-        <div v-else @dblclick="onDblClicK(item)" class="item">
-          <!-- [{{ item.id.toString().slice(-4) }}] -->
-          {{ item.name }}
-          <!-- ${{ itemTotalCost(item) }}{{ item.info }} -->
-          <span @click="removeItem(item)" class="itemRemove">&#215;</span>
-          <span @click="insertItem(item); item.open = true" class="insertItem">+</span>
-        </div>
+  <template v-for="item in items">
+    <div v-if="isInOpenFolder(item)" :style="{ 'padding-left': item.level * 2 + 'vh' }" class="itemLine" ref="itemref"
+      :class="{ itemLineDragging: item.dragging }, itemHighlight(item)" @mousedown="onMouseDown($event, item)"
+      @mousemove="onMouseMove($event, item)" @touchstart="onMouseDown($event, item)"
+      @touchmove="onDragOver($event, item)">
+      <span :class="{ invisible: !isFolder(item) }" @click="toggleOpenFolder(item)" class="isFolder">
+        {{ item.open === false ? '&#62;' : '&#709;' }}</span>
+      <input v-if="state.itemEditing == item.id" v-model="item.name" @blur="state.itemEditing = false"
+        @keyup.esc="state.itemEditing = false" @keyup.enter="state.itemEditing = false" id="itemEditInput" />
+      <div v-else class="item">
+        <!-- [{{ item.id.toString().slice(-4) }}] -->
+        <span @dblclick="onDblClicK(item)">{{ item.name }}</span>
+        <!-- ${{ itemTotalCost(item) }}{{ item.info }} -->
       </div>
-    </template>
-    <!-- {{ drag }} -->
-  </div>
+      <div v-if="item.highlight == 1" class="itemEdition">
+        <span @click="removeItem(item)" class="itemRemove">&times;</span>
+        <span @click="insertItem(item); item.open = true" class="insertItem">+</span>
+      </div>
+    </div>
+  </template>
+  <!-- {{ state }}<br />{{ drag }} -->
 </template>
 
 <style>
-.dragging {
-  background-color: rgb(241, 255, 150);
-}
-
 .itemLine {
-  /* border: 2px solid transparent; */
-  /* font-size: 2.5vh; */
+  display: flex;
   white-space: nowrap;
   text-overflow: ellipsis;
   border-bottom: 1px solid rgb(228, 232, 199);
   height: 3vh;
+  padding: .25vh 0;
+  cursor: default;
+}
+
+.itemLineDragging {
+  background-color: rgba(241, 255, 150, 0.8);
+  cursor: ns-resize;
 }
 
 .item {
-  display: inline;
   overflow: hidden;
-}
-
-.itemLine:hover .item span {
-  visibility: visible;
+  align-self: center;
 }
 
 .invisible {
@@ -300,25 +294,40 @@ function onMouseMove(event) {
 }
 
 .isFolder {
+  vertical-align: bottom;
   cursor: pointer;
   color: goldenrod;
 }
 
 .isFolder:hover {
-  color: unset;
+  color: rgb(128, 109, 0);
+}
+
+.itemEdition {
+  margin-left: auto;
+  margin-right: .5vh;
+  font-size: 1.5em;
+  text-shadow: -1px -1px 0 rgba(0, 0, 0, 0.5), 1px -1px 0 rgba(0, 0, 0, 0.5),
+    -1px 1px 0 rgba(0, 0, 0, 0.5), 1px 1px 0 rgba(0, 0, 0, 0.5);
 }
 
 .itemRemove {
-  visibility: hidden;
   cursor: pointer;
   margin-left: .5vh;
   color: red;
 }
 
 .insertItem {
-  visibility: hidden;
   cursor: pointer;
-  margin-left: .25vh;
+  margin-left: .5vh;
   color: green;
+}
+
+.itemHighlight1 {
+  background-color: rgba(241, 255, 150, 0.6);
+}
+
+.itemHighlight2 {
+  background-color: rgba(241, 255, 150, 0.3);
 }
 </style>
